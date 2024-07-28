a4f46842992946b3ae87d706900f3fbe206bc097
Bhawana Pal (EXT-Nokia) <bhawana.pal.ext@nokia.com>
2024-07-29 01:07:50 +0530

    two rgs rg1 and rg2

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "rg1"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "rg2"
+  location = "centralindia"
+}
